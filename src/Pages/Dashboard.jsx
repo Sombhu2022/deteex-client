89f8d26ea08f1fc28ba727eb73b4dcd608453ea7
Pritam <123.paulpritam@gmail.com>
2024-03-08 17:59:50 +0530

    Sidebar & setup done

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,9 +3,17 @@ import Searchbar from "../Components/Searchbar";
 
 const Dashboard = () => {
     return (
-        <div>
-            <Searchbar />
-        </div>
+        <section id="dashboard" className="w-full px-3 py-2">
+            <header className="flex items-center justify-between flex-row">
+                <div className="userDetails flex items-start flex-col">
+                    <h1 className="text-purple-800 font-semibold">
+                        Welcome back, John...
+                    </h1>
+                    <small>Manager</small>
+                </div>
+                <Searchbar />
+            </header>
+        </section>
     );
 };
 
