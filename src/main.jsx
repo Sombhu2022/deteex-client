9a19c3aa95b758f7aef7f69464198b20df1bd5eb
saniyaj mallik <saniyajmallik12@gmail.com>
2024-03-13 20:03:52 +0530

    o auth added

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Router>
-        <App />
-    </Router>
+	<Router>
+		<GoogleOAuthProvider clientId='181727105626-juff5vov439lbm54s55hjo0pstnmqas7.apps.googleusercontent.com'>
+			<App />
+		</GoogleOAuthProvider>
+	</Router>
 );
