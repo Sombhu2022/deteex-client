3a632419330420b1ebeb97d647176bbcab9b71af
saniyaj mallik <saniyajmallik12@gmail.com>
2024-03-23 20:31:03 +0530

    worker added

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 // slices
 import userReducer from './slices/authSlice'
 import companyReducer from './slices/companySlice'
+import employeeReducer from './slices/employeeSlice'
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         company: companyReducer,
+        employee: employeeReducer,
 
     }
 }) 
